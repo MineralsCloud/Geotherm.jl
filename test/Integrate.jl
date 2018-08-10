ac014a5913782fe9658210ee930f4a11f11b402a
Qi Zhang <singularitti@outlook.com>
2018-08-10 00:04:50 -0400

    Change keyword arg to optional arg

--- test/Integrate.jl
+++ test/Integrate.jl
@@ -12,4 +12,4 @@ using GeoTherm
 @test runge_kutta_iter(Point(1.0, 1.0), (x, y) -> x * 2 + y * 3) == Point{Float64}(1.01, 1.05085856375)
 
 # @show runge_kutta(Point(1.0, 1.0), (x, y) -> x * 2 + y * 3; nstep=4)
-@test runge_kutta(Point(1.0, 1.0), (x, y) -> x * 2 + y * 3; nstep=4) == Point{Float64}[Point{Float64}(1.0, 1.0), Point{Float64}(1.01, 1.05086), Point{Float64}(1.02, 1.10347), Point{Float64}(1.03, 1.15788)]
+@test runge_kutta(Point(1.0, 1.0), (x, y) -> x * 2 + y * 3, 0.01, 4) == [Point{Float64}(1.0, 1.0), Point{Float64}(1.01, 1.05086), Point{Float64}(1.02, 1.10347), Point{Float64}(1.03, 1.15788)]
