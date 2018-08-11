c110b435dcc3eb2c6978de88339ffdad4592221c
Qi Zhang <singularitti@outlook.com>
2018-08-11 01:15:09 -0400

    docs: Add doc strings for functions

--- src/Interpolate.jl
+++ src/Interpolate.jl
@@ -17,8 +17,19 @@ using Geometry: Point, Rectangle, Manifold, rectangle_to_points, within_rectangl
 
 export bilinear_interpolate
 
+"""
+    linear_interpolate(a, b, f, g)
+
+Return a function of the linear interpolation between any 2 abstract data points `(a, f)` and `(b, g)`.
+"""
 linear_interpolate(a, b, f, g)::Function = x -> ((b - x) * f + (x - a) * g) / (b - a)
 
+"""
+    bilinear_interpolate(p, q, r, s)
+
+If 4 points consisting of a rectangle and their z-coordinates are known, return a bilinear interpolation of
+the 4 points.
+"""
 function bilinear_interpolate(p::Manifold, q::Manifold, r::Manifold, s::Manifold)::Function
     x1, x2, y1, y2 = p.p.x, q.p.x, p.p.y, q.p.y
     z1, z2, z3, z4 = p.z, q.z, r.z, s.z
