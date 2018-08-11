1d655798e4bb3eea51a3e2d006ea2a6f35ad1c7b
Qi Zhang <singularitti@outlook.com>
2018-08-11 02:05:42 -0400

    Fix a bug

--- src/Interpolate.jl
+++ src/Interpolate.jl
@@ -33,7 +33,7 @@ the 4 points.
 function bilinear_interpolate(q11::Manifold, q12::Manifold, q21::Manifold, q22::Manifold)::Function
     x1, x2, y1, y2 = q11.p.x, q21.p.x, q11.p.y, q22.p.y
     v11, v12, v21, v22 = q11.z, q12.z, q21.z, q22.z
-    rec = Rectangle(x1, x2, y1, y2)
+    rec = Rectangle(x1, y1, x2, y2)
     function (x, y)
         within_rectangle(rec, Point(x, y)) || error("The point ($x, $y) is out of boundary!")
         v1 = linear_interpolate(x1, x2, v11, v21)(x)
