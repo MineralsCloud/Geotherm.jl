70168f564ca65d211d9f2d7032e69bd15966a7e6
Qi Zhang <singularitti@outlook.com>
2018-08-11 02:04:32 -0400

    Rename variables
    follow the style of wiki: https://en.wikipedia.org/wiki/Bilinear_interpolation

--- src/Interpolate.jl
+++ src/Interpolate.jl
@@ -25,20 +25,20 @@ Return a function of the linear interpolation between any 2 abstract data points
 linear_interpolate(a, b, f, g)::Function = x -> ((b - x) * f + (x - a) * g) / (b - a)
 
 """
-    bilinear_interpolate(p, q, r, s)
+    bilinear_interpolate(q11, q12, q21, q22)
 
 If 4 points consisting of a rectangle and their z-coordinates are known, return a bilinear interpolation of
 the 4 points.
 """
-function bilinear_interpolate(p::Manifold, q::Manifold, r::Manifold, s::Manifold)::Function
-    x1, x2, y1, y2 = p.p.x, q.p.x, p.p.y, q.p.y
-    z1, z2, z3, z4 = p.z, q.z, r.z, s.z
+function bilinear_interpolate(q11::Manifold, q12::Manifold, q21::Manifold, q22::Manifold)::Function
+    x1, x2, y1, y2 = q11.p.x, q21.p.x, q11.p.y, q22.p.y
+    v11, v12, v21, v22 = q11.z, q12.z, q21.z, q22.z
     rec = Rectangle(x1, x2, y1, y2)
     function (x, y)
         within_rectangle(rec, Point(x, y)) || error("The point ($x, $y) is out of boundary!")
-        f1 = linear_interpolate(x1, x2, z1, z2)
-        f2 = linear_interpolate(x1, x2, z3, z4)
-        linear_interpolate(y1, y2, f1(x), f2(x))(y)
+        v1 = linear_interpolate(x1, x2, v11, v21)(x)
+        v2 = linear_interpolate(x1, x2, v12, v22)(x)
+        linear_interpolate(y1, y2, v1, v2)(y)
     end
 end
 
