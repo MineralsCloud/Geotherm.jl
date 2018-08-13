cd36277d512a3894aff674e217b1630db8d7afdf
Qi Zhang <singularitti@outlook.com>
2018-08-13 10:50:11 -0400

    Fix a possible bug

--- src/Geometry.jl
+++ src/Geometry.jl
@@ -38,6 +38,8 @@ struct SurfacePoint{T <: Real}
     z::T
 end
 
+SurfacePoint(x::T, y::T, z::T) where T <: Real = SurfacePoint{T}(x, y, z)
+
 function rectangle_to_points(rec::Rectangle{T})::NTuple{4, Point{T}} where T <: Real
     lx, rx, ly, ry = rec.lx, rec.ly, rec.rx, rec.ry
     Point(lx, ly), Point(lx, ry), Point(rx, ly), Point(rx, ry)
@@ -46,7 +48,7 @@ end
 function within_rectangle(rec::Rectangle{T}, t::Point{T})::Bool where T <: Real
     lx, rx, ly, ry = rec.lx, rec.ly, rec.rx, rec.ry
     x, y = t.x, t.y
-    lx < x < rx && ly < y < ry
+    lx <= x <= rx && ly <= y <= ry
 end
 
 function point_to_surface_point(p::Point{T}, z::T) where T <: Real
