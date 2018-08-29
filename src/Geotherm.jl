e3bbe7d86c0b52bc6a23e62211091d00a1cbfbdb
Qi Zhang <singularitti@outlook.com>
2018-08-29 05:02:31 -0400

    Remove Plot.jl and its unit test
    I do not want an incomplete plotting feature, especially
    in Julia

--- src/Geotherm.jl
+++ src/Geotherm.jl
@@ -19,12 +19,10 @@ include("Geometry.jl")
 include("Interpolate.jl")
 include("Integrate.jl")
 include("Bind.jl")
-include("Plot.jl")
 
 @reexport using Geotherm.Geometry
 @reexport using Geotherm.Interpolate
 @reexport using Geotherm.Integrate
 @reexport using Geotherm.Bind
-@reexport using Geotherm.Plot
 
 end
--- test/Bind.jl
+++ test/Bind.jl
@@ -8,5 +8,3 @@ Bind:
 using Test
 
 using Geotherm: bind, Point, Rectangle
-
-@show bind(rand(1.0:100.0, 100, 100), collect(0.0:0.1:1000.0), collect(0.0:0.1:1000.0), Point(1.05, 1.02), 0.01, 10)
