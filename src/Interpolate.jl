771286ec4099bae25d209ecbdedd9761507cc0eb
Qi Zhang <singularitti@outlook.com>
2018-08-29 03:53:16 -0400

    Add error check for `linear_interpolate` function

--- src/Interpolate.jl
+++ src/Interpolate.jl
@@ -22,7 +22,11 @@ export bilinear_interpolate
 
 Return a function of the linear interpolation between any 2 abstract data points `(a, f)` and `(b, g)`.
 """
-linear_interpolate(a, b, f, g)::Function = x -> ((b - x) * f + (x - a) * g) / (b - a)
+function linear_interpolate(a, b, f, g)::Function
+    a != b || error("The argument `a` cannot equal to the argument `b`!")
+
+    x -> ((b - x) * f + (x - a) * g) / (b - a)
+end
 
 """
     bilinear_interpolate(q11, q12, q21, q22)
@@ -34,8 +38,9 @@ function bilinear_interpolate(q11::T, q12::T, q21::T, q22::T)::Function where T 
     x1, x2, y1, y2 = q11.x, q21.x, q11.y, q22.y
     v11, v12, v21, v22 = q11.z, q12.z, q21.z, q22.z
     rec = Rectangle(x1, y1, x2, y2)
+
     function (x, y)
-        within_rectangle(rec, Point(x, y)) || error("The point ($x, $y) is out of boundary!")
+        within_rectangle(rec, Point(x, y)) || error("The point ($x, $y) is out of boundary $rec!")
         v1 = linear_interpolate(x1, x2, v11, v21)(x)
         v2 = linear_interpolate(x1, x2, v12, v22)(x)
         linear_interpolate(y1, y2, v1, v2)(y)
