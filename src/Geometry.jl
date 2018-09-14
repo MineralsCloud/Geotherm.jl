1d2c681894de4ed01f6582e0bcd3165124475f67
Qi Zhang <singularitti@outlook.com>
2018-09-13 23:37:58 -0400

    Implement new data structure: `CartesianCoordinates`

--- src/Geometry.jl
+++ src/Geometry.jl
@@ -11,19 +11,9 @@ julia>
 """
 module Geometry
 
-export Point, Rectangle, SurfacePoint, Mapping, rectangle_to_points, within_rectangle
+export CartesianCoordinates, Rectangle, rectangle_to_points, within_rectangle
 
-abstract type AbstractPoint end
-
-struct Point{T <: Real} <: AbstractPoint
-    x::T
-    y::T
-end
-
-struct Mapping <: AbstractPoint
-    x
-    y
-end
+CartesianCoordinates(x::T, y::T) where T <: Real = NamedTuple{(:x, :y), Tuple{T, T}}((x, y))
 
 struct Rectangle{T <: Real}
     lx::T
@@ -39,14 +29,6 @@ struct Rectangle{T <: Real}
 end
 
 Rectangle(lx::T, rx::T, ly::T, uy::T) where T <: Real = Rectangle{T}(lx, rx, ly, uy)
-
-struct SurfacePoint{T <: Real}
-    x::T
-    y::T
-    z::T
-end
-
-SurfacePoint(x::T, y::T, z::T) where T <: Real = SurfacePoint{T}(x, y, z)
 
 function rectangle_to_points(rec::Rectangle{T})::NTuple{4, Point{T}} where T <: Real
     lx, rx, ly, uy = rec.lx, rec.rx, rec.ly, rec.uy
