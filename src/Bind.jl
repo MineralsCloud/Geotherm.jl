0c694e17953d6ad3fb5d782d3cf486208db0eaa0
Qi Zhang <singularitti@outlook.com>
2018-12-21 16:25:14 -0500

    Use `Point` in Bind.jl

--- src/Bind.jl
+++ src/Bind.jl
@@ -15,7 +15,7 @@ module Bind
 using BisectPy: bisect_right
 using DataFrames
 
-using Geotherm.Geometry: Point, Rectangle, SurfacePoint
+using Geotherm.Geometry: Point2D, Point3D, Rectangle
 using Geotherm.Interpolate: bilinear_interpolate
 using Geotherm.Integrate: runge_kutta_iter
 
@@ -30,20 +30,20 @@ function inject_find_lower_bound(ps, ts, geothermal_gradient)
         m, n = find_lower_bounds(ps, ts)(x, y)
         o, p = m + 1, n + 1
         interpolated_function = bilinear_interpolate(
-            SurfacePoint(ps[m], ts[n], geothermal_gradient[n, m]),  # Note the order of indices!
-            SurfacePoint(ps[m], ts[p], geothermal_gradient[p, m]),  # Note the order of indices!
-            SurfacePoint(ps[o], ts[n], geothermal_gradient[n, o]),  # Note the order of indices!
-            SurfacePoint(ps[o], ts[p], geothermal_gradient[p, o])  # Note the order of indices!
+            Point3D(ps[m], ts[n], geothermal_gradient[n, m]),  # Note the order of indices!
+            Point3D(ps[m], ts[p], geothermal_gradient[p, m]),  # Note the order of indices!
+            Point3D(ps[o], ts[n], geothermal_gradient[n, o]),  # Note the order of indices!
+            Point3D(ps[o], ts[p], geothermal_gradient[p, o])  # Note the order of indices!
         )
         interpolated_function(x, y)
     end
 end
 
-function generate_trace(geothermal_gradient::DataFrame, p0::Point{T}, h=0.01, n=1000) where T <: Real
+function generate_trace(geothermal_gradient::DataFrame, p0::Point2D, h=0.01, n=1000) where T <: Real
     ps = float(names(geothermal_gradient))
     ts = float(geothermal_gradient[Symbol("T(K)\P(GPa)")])
 
-    trace = Point{T}[]
+    trace = Point2D{T}[]
     push!(trace, p0)
     f = inject_find_lower_bound(ps, ts, geothermal_gradient)
     for i = 1:(n - 1)
