8a01962e8237bbb9f6d4082dbb4192ed6e3447ee
Qi Zhang <singularitti@outlook.com>
2019-06-05 01:28:40 -0400

    Update test test/Integrate.jl

--- test/Integrate.jl
+++ test/Integrate.jl
@@ -12,5 +12,5 @@ using Geotherm.Integrate
 
 @testset "Test `runge_kutta`" begin
     @test runge_kutta_iter(Point2D(1.0, 1.0), (x, y) -> x * 2 + y * 3) == Point2D(1.01, 1.05085856375)
-    @show runge_kutta(Point2D(1.0, 1.0), (x, y) -> x * 2 + y * 3, 0.01, 4) == [Point2D(1.0, 1.0), Point2D(1.01, 1.05086), Point2D(1.02, 1.10347), Point2D(1.03, 1.15788)]
+    @test runge_kutta(Point2D(1.0, 1.0), (x, y) -> x * 2 + y * 3, 0.01, 4) == [Point2D(1.0, 1.0), Point2D(1.01, 1.05085856375), Point2D(1.02, 1.103469031571201), Point2D(1.03, 1.157884756885266)]
 end
