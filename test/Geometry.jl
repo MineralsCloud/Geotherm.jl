62f4bc8aeb951c783ecfc833537bec09493e877a
Qi Zhang <singularitti@outlook.com>
2019-06-05 01:15:49 -0400

    test: Fix style

--- test/Geometry.jl
+++ test/Geometry.jl
@@ -9,12 +9,10 @@ using Test
 
 using Geotherm
 
-@test in(Point2D(6, 4), Rectangle(1, 10, 5, 20)) == false
-
-@test in(Point2D(6, 15), Rectangle(1, 10, 5, 20)) == true
-
-@test in(Point2D(5, 5), Rectangle(1, 10, 5, 20)) == true
-
-@test in(Point2D(1, 15), Rectangle(1, 10, 5, 20)) == true
-
-@test in(Point2D(1, 5), Rectangle(1, 10, 5, 20)) == true
+@testset "Test whether a point is in the rectangle" begin
+    @test in(Point2D(6, 4), Rectangle(1, 10, 5, 20)) == false
+    @test in(Point2D(6, 15), Rectangle(1, 10, 5, 20)) == true
+    @test in(Point2D(5, 5), Rectangle(1, 10, 5, 20)) == true
+    @test in(Point2D(1, 15), Rectangle(1, 10, 5, 20)) == true
+    @test in(Point2D(1, 5), Rectangle(1, 10, 5, 20)) == true
+end
--- test/Integrate.jl
+++ test/Integrate.jl
@@ -7,8 +7,10 @@ Integrate:
 
 using Test
 
+using Geotherm
 using Geotherm.Integrate
 
-@test runge_kutta_iter(Point2D(1.0, 1.0), (x, y) -> x * 2 + y * 3) == Point2D(1.01, 1.05085856375)
-
-@test runge_kutta(Point2D(1.0, 1.0), (x, y) -> x * 2 + y * 3, 0.01, 4) == [Point2D(1.0, 1.0), Point2D(1.01, 1.05086), Point2D(1.02, 1.10347), Point2D(1.03, 1.15788)]
+@testset "Test `runge_kutta`" begin
+    @test runge_kutta_iter(Point2D(1.0, 1.0), (x, y) -> x * 2 + y * 3) == Point2D(1.01, 1.05085856375)
+    @show runge_kutta(Point2D(1.0, 1.0), (x, y) -> x * 2 + y * 3, 0.01, 4) == [Point2D(1.0, 1.0), Point2D(1.01, 1.05086), Point2D(1.02, 1.10347), Point2D(1.03, 1.15788)]
+end
