b48f4f1a4b0420afd8c653445bc2a3e0a3899eb4
Qi Zhang <singularitti@outlook.com>
2019-12-18 17:50:40 -0500

    Deprecate `@reexport` of Reexport.jl

--- src/Geotherm.jl
+++ src/Geotherm.jl
@@ -11,16 +11,9 @@ julia>
 """
 module Geotherm
 
-using Reexport
-
 include("Geometry.jl")
 include("Interpolate.jl")
 include("Integrate.jl")
 include("Bind.jl")
-
-@reexport using Geotherm.Geometry
-@reexport using Geotherm.Interpolate
-@reexport using Geotherm.Integrate
-@reexport using Geotherm.Bind
 
 end
