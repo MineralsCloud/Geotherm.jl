2c6acc117895679f1ab7d18dcd465f5aebb92a0b
singularitti <singularitti@outlook.com>
2021-03-17 00:44:53 -0400

    Use subscripts in src/Interpolate.jl

--- src/Interpolate.jl
+++ src/Interpolate.jl
@@ -18,31 +18,30 @@ export linear_interpolate, bilinear_interpolate
 """
     linear_interpolate(a, b)
 
-Return a function of the linear interpolation between any 2 points `(x1, f(x1))` and `(x2, g(x2))`.
+Return a function of the linear interpolation between any 2 points `(x₁, f(x₁))` and `(x₂, g(x₂))`.
 """
 function linear_interpolate(a::Point2D, b::Point2D)
-    x1, y1, x2, y2 = a.x, a.y, b.x, b.y
-    @assert x1 != x2 "The x-coordinates of the 2 arguments `a` and `b` cannot be equal!"
-    return x -> ((x2 - x) * y1 + (x - x1) * y2) / (x2 - x1)
+    x₁, y₁, x₂, y₂ = a.x, a.y, b.x, b.y
+    @assert x₁ != x₂ "The x-coordinates of the 2 arguments `a` and `b` cannot be equal!"
+    return x -> ((x₂ - x) * y₁ + (x - x₁) * y₂) / (x₂ - x₁)
 end
 
 """
-    bilinear_interpolate(q11, q12, q21, q22)
+    bilinear_interpolate(q₁₁, q₁₂, q₂₁, q₂₂)
 
-If 4 points consisting of a rectangle and their z-coordinates are known, return a bilinear interpolation of
-the 4 points.
+If 4 points consisting of a rectangle and their z-coordinates are known, return a bilinear interpolation of the 4 points.
 """
-function bilinear_interpolate(q11::T, q12::T, q21::T, q22::T) where {T<:Point3D}
-    x1, x2, y1, y2 = q11.x, q21.x, q11.y, q22.y
-    z11, z12, z21, z22 = map(q -> q.z, (q11, q12, q21, q22))
-    rec = Rectangle(x1, y1, x2, y2)
+function bilinear_interpolate(q₁₁::T, q₁₂::T, q₂₁::T, q₂₂::T) where {T<:Point3D}
+    x₁, x₂, y₁, y₂ = q₁₁.x, q₂₁.x, q₁₁.y, q₂₂.y
+    z₁₁, z₁₂, z₂₁, z₂₂ = map(q -> q.z, (q₁₁, q₁₂, q₂₁, q₂₂))
+    rec = Rectangle(x₁, y₁, x₂, y₂)
     return function (x, y)
         if Point2D(x, y) ∉ rec
             error("point ($x, $y) is out of boundary $rec.")
         end
-        v1 = linear_interpolate(Point2D(x1, z11), Point2D(x2, z21))(x)
-        v2 = linear_interpolate(Point2D(x1, z12), Point2D(x2, z22))(x)
-        return linear_interpolate(Point2D(y1, v1), Point2D(y2, v2))(y)
+        v₁ = linear_interpolate(Point2D(x₁, z₁₁), Point2D(x₂, z₂₁))(x)
+        v₂ = linear_interpolate(Point2D(x₁, z₁₂), Point2D(x₂, z₂₂))(x)
+        return linear_interpolate(Point2D(y₁, v₁), Point2D(y₂, v₂))(y)
     end
 end
 
