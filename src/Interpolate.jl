a9eadff89f8da2f59ab4d815b9c541ef430b1fba
singularitti <singularitti@outlook.com>
2021-03-17 00:39:46 -0400

    Simplify module `Interpolate`

--- src/Interpolate.jl
+++ src/Interpolate.jl
@@ -11,8 +11,6 @@ julia>
 """
 module Interpolate
 
-using ExtractMacro: @extract
-
 using Geotherm.Geometry: Point2D, Point3D, Rectangle
 
 export linear_interpolate, bilinear_interpolate
@@ -23,9 +21,8 @@ export linear_interpolate, bilinear_interpolate
 Return a function of the linear interpolation between any 2 points `(x1, f(x1))` and `(x2, g(x2))`.
 """
 function linear_interpolate(a::Point2D, b::Point2D)
-    @extract a x1 = x y1 = y  # x1, y1 = a.x, a.y
-    @extract b x2 = x y2 = y  # x2, y2 = b.x, b.y
-    @assert(x1 != x2, "The x-coordinates of the 2 arguments `a` and `b` cannot be equal!")
+    x1, y1, x2, y2 = a.x, a.y, b.x, b.y
+    @assert x1 != x2 "The x-coordinates of the 2 arguments `a` and `b` cannot be equal!"
     return x -> ((x2 - x) * y1 + (x - x1) * y2) / (x2 - x1)
 end
 
@@ -36,12 +33,13 @@ If 4 points consisting of a rectangle and their z-coordinates are known, return 
 the 4 points.
 """
 function bilinear_interpolate(q11::T, q12::T, q21::T, q22::T) where {T<:Point3D}
-    x1, x2 = q11.x, q21.x
-    y1, y2 = q11.y, q22.y
-    z11, z12, z21, z22 = [q11, q12, q21, q22] |> (q -> q.z)
+    x1, x2, y1, y2 = q11.x, q21.x, q11.y, q22.y
+    z11, z12, z21, z22 = map(q -> q.z, (q11, q12, q21, q22))
     rec = Rectangle(x1, y1, x2, y2)
     return function (x, y)
-        Point2D(x, y) in rec || error("The point ($x, $y) is out of boundary $rec!")
+        if Point2D(x, y) ∉ rec
+            error("point ($x, $y) is out of boundary $rec.")
+        end
         v1 = linear_interpolate(Point2D(x1, z11), Point2D(x2, z21))(x)
         v2 = linear_interpolate(Point2D(x1, z12), Point2D(x2, z22))(x)
         return linear_interpolate(Point2D(y1, v1), Point2D(y2, v2))(y)
