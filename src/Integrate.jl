f7c2446156b0406c3236e37b65a3ce49de64874d
singularitti <singularitti@outlook.com>
2021-03-17 00:46:28 -0400

    Deprecate `@extract` in `runge_kutta_iter`

--- src/Integrate.jl
+++ src/Integrate.jl
@@ -11,15 +11,13 @@ julia>
 """
 module Integrate
 
-using ExtractMacro: @extract
-
 using Geotherm.Geometry: Point2D
 
 export runge_kutta
 
 # This is a helper function and should not be exported.
 function runge_kutta_iter(p::Point2D, f, h = 0.01)
-    @extract p x y  # x, y = p.x, p.y
+    x, y = p.x, p.y
     k1 = h * f(x, y)
     k2 = h * f(x + h / 2, y + k1 / 2)
     k3 = h * f(x + h / 2, y + k2 / 2)
